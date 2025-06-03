22f495e4760f54876701602cbdae5c2da31400f2
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2025-06-03 20:58:34 +0800

    初始化schedule

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import React, { useState } from 'react';
+import EmployeeForm from './components/Employ';
+
 function App() {
+  const [employeeList, setEmployeeList] = useState([])
+
   return (
-    <div className="App">
-      test
+    <div className="p-6">
+      <EmployeeForm onEmployeeListChange={setEmployeeList} />
+      {/* 後面還可以加 排班規則設定表單、產生按鈕、FullCalendar */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
